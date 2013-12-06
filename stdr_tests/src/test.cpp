e40c3ad7873afbed5c0d620256e289df65154996
Manos Tsardoulias <etsardou@gmail.com>
2013-12-06 22:23:32 +0200

    Initial commit for testing. Does not build. Cant find gui headers

---
+++ stdr_tests/src/test.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+
+//~ #include <stdr_gui/stdr_gui_sensors/stdr_gui_sonar.h>
+#include <stdr_robot/handle_robot.h>
+#include <stdr_gui/stdr_tools.h>
+
+int main(void){
+	return 0;
+}
