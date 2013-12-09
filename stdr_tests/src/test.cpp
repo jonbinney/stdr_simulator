7b8af09ff90279d37cce3bc873c26294e4e4c7ea
Manos Tsardoulias <etsardou@gmail.com>
2013-12-09 16:52:57 +0200

    Build fixed

--- stdr_tests/src/test.cpp
+++ stdr_tests/src/test.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
 #include "gtest/gtest.h"
-#include "gmock/gmock.h"
+#include <gmock/gmock.h>
 
-//~ #include <stdr_gui/stdr_gui_sensors/stdr_gui_sonar.h>
 #include <stdr_robot/handle_robot.h>
 #include <stdr_gui/stdr_tools.h>
 
